90ae1cb362f06c7270377efcf63ccc010130ccaa
Aryan Chegini <cheginiaryan.ac@gmail.com>
2023-08-06 21:49:54 +0100

    Completed chapter 4 exercises

--- ch04.js
+++ ch04.js
@@ -1,49 +1,119 @@
-// //Reversing An Array:
+//The Sum Of A Range:
+function range(start, end, step = start < end ? 1  : -1) {
+    let arr = [];
+    if (step > 0) {
+        for (let i = start; i <= end; i += step) arr.push(i);
+    } else {
+        for (let i = start; i >= end; i += step) arr.push(i);
+    }
+    return arr;
+}
 
-// function reverseArray(arr) {
-//   let newArr = [];
-//   for (let item of arr) {
-//     newArr.unshift(item);
-//   }
-//   return newArr;
-// }
+let sum = (arr) => arr.reduce((total, num) => total += num, 0)
 
-// function reverseArrayInPlace(arr) {
-//   let i = 0, j = arr.length - 1;
-//   while (i<j) {
-//     [arr[i], arr[j]] = [arr[j], arr[i]]; 
-//     i++;
-//     j--;
-//   }
-// }
+//Reversing An Array:
 
-// //A List:
+function reverseArray(arr) {
+  let newArr = [];
+  for (let item of arr) {
+    newArr.unshift(item);
+  }
+  return newArr;
+}
 
+function reverseArrayInPlace(arr) {
+  let i = 0, j = arr.length - 1;
+  while (i<j) {
+    [arr[i], arr[j]] = [arr[j], arr[i]]; 
+    i++;
+    j--;
+  }
+}
+
+//A List:
+
+//VERSION 1:
 // function arrayToList(arr) {
-//   let list = null
-//   for (let i = arr.length - 1; i >= 0; i--) {
-//     list = {value: arr[i], rest: list};
-//   }
-//   return list;
+//     let list = {value: arr[arr.length - 1], rest: null};
+//     for (let i = arr.length - 2; i >= 0; i--) {
+//         list = {value: arr[i], rest: list};
+//     }
+//     return list;
 // }
 
-// function listToArray(list) {
-//   let arr = [];
-//   let section = list;
-//   while (section !== null) {
-//     arr.push(section.value);
-//     section = section.rest;
-//   }
-//   return arr
+//VERSION 2:
+// function arrayToList(arr) {
+//     let rest = null
+//     let list = {}
+//     for (let i = arr.length - 1; i >= 0; i--) {
+//         list = {value: arr[i], rest};
+//         rest = list
+//     }
+//     return list;
 // }
 
-// function prepend(list, element) {
-//   return {value: element, rest: list}
-// }
+//VERSION 3: RECURSIVE!!!!!!!!
 
-// function nth(list, n) {
-//   if (list === null) return undefined;
-//   if (n < 0) return undefined;
-//   if (n == 0) return list.value;
-//   else {return nth(list.rest, n-1)}
-// }
+function arrayToList(arr, list={value: null, rest: null}) {
+    if (arr.length == 1) {
+        list.value = arr[0];
+        return list;
+    } else {
+        list.value = arr[arr.length - 1];
+        return arrayToList(arr.slice(0, (arr.length-1)), {value: null, rest: list});
+    }
+}
+
+function listToArray(list, arr = []) {
+    if (list.rest == null) {
+        arr.push(list.value);
+        return arr;
+    } else {
+        arr.push(list.value);
+        return listToArray(list.rest, arr);
+    }
+}
+
+function prepend(list, element) {
+    return {value: element, rest: list}
+}
+
+function nth(list, number) {
+    if (list === null) return undefined;
+    if (number < 0) return undefined;
+    if (number == 0) {
+        return list.value;
+    } else {
+        return nth(list.rest, number-1);
+    }
+}
+
+// console.log(listToArray(arrayToList([10, 20, 30])));
+// console.log(prepend(10, prepend(20, null)));
+// console.log(nth(arrayToList([10, 20, 30]), 1));
+
+
+// Deep comparison
+
+function deepEqual(a, b) {
+    if (a === b) return true;
+    else if (a == null || b == null) return false; // use == incase undefined is used 
+    else if (typeof a === 'object' && typeof b === 'object') {
+        let keysA = Object.keys(a), keysB = Object.keys(b);
+        if (keysA.length !== keysB.length) return false;
+
+        for (let key of keysA) {
+            if (!deepEqual(a[key], b[key])) return false;    
+        }
+        return true;
+    } else return false;
+}
+
+
+// let obj = {here: {is: "an"}, object: 2};
+// console.log(deepEqual(obj, obj));
+// // → true
+// console.log(deepEqual(obj, {here: 1, object: 2}));
+// // → false
+// console.log(deepEqual(obj, {here: {is: "an"}, object: 2}));
+// // → true
